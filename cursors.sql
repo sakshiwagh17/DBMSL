2847f43d17d8614055970d9f721ebb76124f582d
Sakshi Wagh <sakshikwagh17@gmail.com>
2024-11-05 18:12:50 +0530

    SQL CURSORS

--- cursors.sql
+++ cursors.sql
@@ -1,36 +1,36 @@
 -- CREATE DATABASE std;
 -- USE std;
--- CREATE TABLE student(rollno int,name varchar(50));
--- INSERT INTO student(rollno,name)
+-- CREATE TABLE o_rollcall(roll_no int,name varchar(50));
+-- INSERT INTO student(roll_no,name)
 -- VALUES
--- (1,"SAKSHI");
--- CREATE TABLE nstudent(rollno int,name varchar(50));
+-- (1,"SAKSHI"),
+-- (2,"SAYALI");
+-- CREATE TABLE n_rollcall(roll_no int,name varchar(50));
 
 DELIMITER //
-CREATE PROCEDURE merge_data(IN rollnew INTEGER) 
+CREATE PROCEDURE merge_data(IN rollnew INTEGER)
 BEGIN
 	DECLARE sroll INTEGER;
 	DECLARE rowcount INTEGER DEFAULT 0;
 	DECLARE terminate BOOLEAN DEFAULT FALSE;
-	DECLARE stud_details CURSOR FOR 
-	SELECT rollno FROM student WHERE rollno=rollnew;
+	DECLARE stud_details CURSOR FOR SELECT roll_no FROM o_rollcall WHERE roll_no=rollnew;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminate=TRUE;
 	OPEN stud_details;
-	getstud:Loop
+	getstud: LOOP
 		FETCH stud_details INTO sroll;
-		IF NOT EXISTS (SELECT * FROM nstudent WHERE rollno=sroll) THEN
-		INSERT INTO nstudent SELECT * FROM student WHERE roll=sroll;
-		SET rowcount=rowcount+1;
-	END IF;
-	IF terminate=TRUE THEN
-		LEAVE getstud;
-	END IF;
+		IF NOT EXISTS (SELECT * FROM n_rollcall WHERE sroll=roll_no) THEN
+			INSERT INTO n_rollcall SELECT * FROM o_rollcall WHERE sroll=roll_no ;
+			SET rowcount=rowcount+1;
+		END IF;
+		IF terminate=TRUE THEN
+			LEAVE getstud;
+		END IF;
 	END LOOP getstud;
-	close stud_details;
-	SELECT CONCAT('RECORD MERGED,',rowcount,'row(s) affected') AS MESSAGE;
-END 
+	CLOSE stud_details;
+SELECT CONCAT("MERGE DATA ",rowcount," row(s) affected") AS Messege;
+END ;
 //
 DELIMITER ;
 
 -- CALL merge_Data(1);
--- SELECT * FROM nstudent;
+-- SELECT * FROM n_rollcall;
