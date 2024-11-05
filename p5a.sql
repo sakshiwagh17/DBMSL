708ce27565c240bb83f8d2ba079130aa17fe82d4
Sakshi Wagh <sakshikwagh17@gmail.com>
2024-11-05 20:03:18 +0530

    updated code

--- p5a.sql
+++ p5a.sql
@@ -1,30 +1,29 @@
 -- run p5b.sql first
 -- CREATE DATABASE class;
 -- USE class;
--- CREATE TABLE stud_marks(name varchar(50),total_marks int);
--- INSERT INTO stud_marks(name,total_marks)
+-- CREATE TABLE stud_marks(roll int,std_name varchar(50),total_marks int);
+-- INSERT INTO stud_marks(roll,name,total_marks)
 -- VALUES
--- ("sakshi",100);
+-- ("1","sakshi",100);
 -- SELECT * FROM stud_marks;
 -- CREATE TABLE result(roll int,name varchar(50),class varchar(50));
 
 DELIMITER //
-CREATE PROCEDURE proc_grade(IN rollnew int)
+CREATE PROCEDURE procclass(IN rollnew INTEGER)
 BEGIN
-DECLARE Tmarks int;
-DECLARE Sname varchar(25);
-DECLARE Sresult varchar(25);
-
-DECLARE EXIT HANDLER FOR NOT FOUND
+    DECLARE tmarks INTEGER;
+    DECLARE sname varchar(50);
+    DECLARE sresult varchar(25);
+    DECLARE EXIT HANDLER FOR NOT FOUND
 BEGIN
-	SELECT CONCAT("ROLLNO",rollnew,"NOT FOUND") AS NoRecord;
+	SELECT CONCAT("ROLL NO ",rollnew," NOT FOUND") AS NoRecord;
 END;
-	SELECT name,total_marks INTO Sname,Sresult FROM stud_marks;
-    SET Sresult=cal_grade(Tmarks);
-    INSERT INTO result VALUES(rollnew,Sname,Sresult);
+	SELECT std_name , total_marks INTO sname,tmarks FROM stud_marks WHERE rollnew=roll;
+    SET sresult=proc_grade(tmarks);
+    INSERT INTO result VALUES(rollnew,sname,sresult);
 END;
 //
 DELIMITER ;
 
--- CALL proc_grade(1);
+-- CALL procclass(1);
 -- SELECT * FROM result;
